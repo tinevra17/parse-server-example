535186588e7733664110e06147ddc8c5cb89b8c6
Samuel Tinevra <tinevra17@hotmail.com>
2019-10-04 10:40:53 -0600

    sam

--- index.js
+++ index.js
@@ -4,6 +4,45 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+var Parse = require('parse/node');
+
+Parse.initialize("constructionID");
+//javascriptKey is required only if you have it on server.
+
+Parse.serverURL = 'http://tinevra.herokuapp.com/parse'
+
+
+// const User = Parse.Object.extend("User");
+// const user = new GameScore();
+
+// user.set("username", "");
+// user.set("pasword", "Sean Plott");
+// user.set("email", "false");
+// user.set("persmission", 0);
+
+// user.save()
+// .then((user) => {
+//   // Execute any logic that should take place after the object is saved.
+//   alert('New object created with objectId: ' + user.id);
+// }, (error) => {
+//   // Execute any logic that should take place if the save fails.
+//   // error is a Parse.Error with an error code and message.
+//   alert('Failed to create new object, with error code: ' + error.message);
+// });
+
+
+
+
+
+
+// app.use('/parse', api);
+
+// var port = 1337;
+// app.listen(port, function() {
+//     console.log('parse-server running on port ' + port);
+// });
+
+
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
@@ -15,8 +54,8 @@ var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  masterKey: process.env.MASTER_KEY || 'tinevra', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'https://tinevra.herokuapp.com',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
@@ -36,7 +75,7 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+  res.status(200).send('sam');
 });
 
 // There will be a test page available on the /test path of your server url
