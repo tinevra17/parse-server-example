7755156d8eae5f66abfa9c3378683624ec68e92a
Samuel Tinevra <tinevra17@hotmail.com>
2019-11-15 11:44:16 -0700

    added the json parser

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 var Parse = require('parse/node');
+var bodyParser = require('body-parser')
 
 Parse.initialize("constructionID","tinevra");
 //javascriptKey is required only if you have it on server.
@@ -42,6 +43,10 @@ var app = express();
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 // app.use(mountPath, api);
@@ -54,6 +59,33 @@ app.get('/user', function(req, res) {
 
 app.get('/tickets', function(req, res) {
   res.status(200).send('sam');
+});
+
+
+app.post('/tickets', function(req, res) {
+  console.log(req.body);
+  //res.send(req.body);
+
+  // const ticket = Parse.Object.extend("Tickets");
+  // const ticket = new GameScore();
+
+  // ticket.set("score", 1337);
+  // ticket.set("playerName", "Sean Plott");
+  // ticket.set("cheatMode", false);
+
+  // ticket.save(
+  // .then((ticket) => {
+  //   // Execute any logic that should take place after the object is saved.
+  //   alert('New object created with objectId: ' + ticket.id);
+  // }, (error) => {
+  //   // Execute any logic that should take place if the save fails.
+  //   // error is a Parse.Error with an error code and message.
+  //   alert('Failed to create new object, with error code: ' + error.message);
+  // });
+
+
+
+  res.status(200).send(req.body);
 });
 
 // There will be a test page available on the /test path of your server url
@@ -83,11 +115,11 @@ app.get('/userCreationTest', function(req, res) {
   const Users = Parse.Object.extend("Users");
   const user = new Users();
   
-  user.set("name", "Daniel Maynez");
-  user.set("username", "daniel123");
-  user.set("email", "danielMay@miners.utep.edu");
-  user.set("password", "daniel");
-  user.set("access",0);
+  user.set("name", "Kenneth Rodriguez");
+  user.set("username", "kenneth");
+  user.set("email", "kenneth@miners.utep.edu");
+  user.set("password", "KennY");
+  user.set("access",3);
   
   user.save()
   .then((user) => {
@@ -111,9 +143,36 @@ res.send("x")
 
 
 
+//retrives user access level as a String based on username and passoword
+//otherwise returns -1
+app.get('/users/:username/:password/accesslvl', function (req, res) {
+  var usernameQuery = new Parse.Query("Users");
+  usernameQuery.equalTo("username", req.params.username);
 
+  var passwordQuery = new Parse.Query("Users");
+  passwordQuery.equalTo("password", req.params.password);
+
+  var mainQuery = Parse.Query.and(usernameQuery,passwordQuery);
+  mainQuery.find().then(function(user) {
+    //gettting the acess level from the user JSON
+    var accesslvl = user[0].get("access");
+    console.log(typeof x);
+
+    res.send(accesslvl+"");
+    // res.sendStatus(status);
+
+  })
+  .catch(function(error) {
+    res.send(error)
+  });
+})
+
+
+
+
+//retrives user JSON based on username and password
+//otherwise returns -1
 app.get('/users/:username/:password', function (req, res) {
-
   var usernameQuery = new Parse.Query("Users");
   usernameQuery.equalTo("username", req.params.username);
 
@@ -134,6 +193,26 @@ app.get('/users/:username/:password', function (req, res) {
   });
 })
 
+
+
+//returns all the users ind the DB inside an array 
+app.get('/usersList', function (req, res) {
+
+  const Users = Parse.Object.extend("Users");
+  const query = new Parse.Query(Users);
+  var usersArr = [];
+
+  query.find().then(function(users) {
+    //populating the array with all the users 
+    users.forEach(user => {
+      usersArr.push(user.get("username"))
+    });
+    res.send(usersArr);
+  })
+  .catch(function(error) {
+    res.send(error)
+  });  
+})
 
 app.listen(3000, () => console.log('express server is running at port # 3000'));
 
